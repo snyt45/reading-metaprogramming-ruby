aaac03ec805a2060d096f971a3066cec9ed12f3f
yuta.sano <snyt45@gmail.com>
2021-10-05 13:04:16 +0900

    2章対応

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -1,8 +1,32 @@
 class TryOut
   # このクラスの仕様
+  attr_accessor :first_name
+  attr_reader :middle_name, :last_name
   # コンストラクタは、2つまたは3つの引数を受け付ける。引数はそれぞれ、ファーストネーム、ミドルネーム、ラストネームの順で、ミドルネームは省略が可能。
+  def initialize(first_name, middle_name=nil, last_name)
+    @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
+  end
+
   # full_nameメソッドを持つ。これは、ファーストネーム、ミドルネーム、ラストネームを半角スペース1つで結合した文字列を返す。ただし、ミドルネームが省略されている場合に、ファーストネームとラストネームの間には1つのスペースしか置かない
+  def full_name
+    return "#{first_name} #{last_name}" if middle_name.nil?
+    "#{first_name} #{middle_name} #{last_name}"
+  end
+
   # first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
+
   # upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
+  def upcase_full_name
+    full_name.upcase
+  end
+
   # upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
+  def upcase_full_name!
+    first_name.upcase!
+    middle_name&.upcase!
+    last_name.upcase!
+    full_name
+  end
 end
--- 02_object_model/hoge_everywhere.rb
+++ 02_object_model/hoge_everywhere.rb
@@ -6,3 +6,62 @@
 # - Class
 # - Hash
 # - TrueClass
+
+# irb(main):001:0> String.ancestors
+# => [String, Comparable, Object, Kernel, BasicObject]
+# irb(main):002:0> Integer.ancestors
+# => [Integer, Numeric, Comparable, Object, Kernel, BasicObject]
+# irb(main):003:0> Numeric.ancestors
+# => [Numeric, Comparable, Object, Kernel, BasicObject]
+# irb(main):004:0> Class.ancestors
+# => [Class, Module, Object, Kernel, BasicObject]
+# irb(main):005:0> Hash.ancestors
+# => [Hash, Enumerable, Object, Kernel, BasicObject]
+# irb(main):006:0> TrueClass.ancestors
+# => [TrueClass, Object, Kernel, BasicObject]
+
+# module KernelRefinements
+#   refine Kernel do
+#     def hoge
+#       "hoge"
+#     end
+#   end
+# end
+
+# using KernelRefinements
+
+class String
+  def hoge
+    "hoge"
+  end
+end
+
+class Integer
+  def hoge
+    "hoge"
+  end
+end
+
+class Numeric
+  def hoge
+    "hoge"
+  end
+end
+
+class Class
+  def hoge
+    "hoge"
+  end
+end
+
+class Hash
+  def hoge
+    "hoge"
+  end
+end
+
+class TrueClass
+  def hoge
+    "hoge"
+  end
+end

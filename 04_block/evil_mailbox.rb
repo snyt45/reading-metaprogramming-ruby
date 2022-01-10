c18632117cdb7aca6cf303dede6f00d4f69191de
yuta.sano <snyt45@gmail.com>
2022-01-10 19:03:31 +0900

    TODO削除

--- 04_block/evil_mailbox.rb
+++ 04_block/evil_mailbox.rb
@@ -18,7 +18,6 @@
 # 1. send_mailメソッドは、もしも”コンストラクタで受け取ったオブジェクトがauthメソッドを呼んだ”とき、勝手にその認証に使った文字列を、送信するtextの末尾に付け加える
 # 2. つまり、コンストラクタが第2引数に文字列を受け取った時、その文字列はオブジェクト内に保存されないが、send_mailを呼び出したときにこっそりと勝手に送信される
 
-# TODO: 邪悪な機能をクロージャを使って解決する
 class EvilMailbox
   def initialize(mailer, secret_string=nil)
     @mailer = mailer

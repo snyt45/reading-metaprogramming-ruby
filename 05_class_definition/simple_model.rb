4c79e68a5455dc8a3114a143798e9c077e69dcc7
yuta.sano <snyt45@gmail.com>
2022-02-28 21:47:05 +0900

    5章のSimpleModel

--- 05_class_definition/simple_model.rb
+++ 05_class_definition/simple_model.rb
@@ -14,4 +14,67 @@
 # 3. 履歴がある場合、すべての操作履歴を放棄し、値も初期状態に戻す `restore!` メソッドを作成する
 
 module SimpleModel
+  def initialize(attr_hash)
+    attr_hash.keys.each do |key|
+      # attr_accessorで作成したアトリビュートと同名のキーがあるとき
+      if methods.include?(key)
+        # @{attr} に初期値をセット
+        instance_variable_set("@#{key.to_s}", attr_hash[key])
+        # @{attr}_changed_history に初期値をセット
+        instance_variable_set("@#{key.to_s}_changed_history", [].push(attr_hash[key]))
+      end
+    end
+  end
+
+  def changed?
+    # @{attr}_changed_history というインスタンス変数を列挙
+    changed_histories = instance_variables.select { |v| v.to_s.end_with?("_changed_history") }
+    # changed_historiesの各インスタンス変数の配列の操作履歴に一個でも2以上があれば変更されているとみなす
+    changed = changed_histories.select { |v| instance_variable_get(v).count >= 2 }
+    !changed.empty?
+  end
+
+  def restore!
+    return unless changed? # 履歴がない場合は早期リターン
+    changed_histories = instance_variables.select { |v| v.to_s.end_with?("_changed_history") }
+    changed_histories.each do |changed_history|
+      # @{attr}_changed_history に初期値をセット
+      default_value = instance_variable_get(changed_history).first
+      instance_variable_set(changed_history, [].push(default_value))
+      # @{attr} に初期値をセット
+      attr_name = changed_history.to_s.slice(/@[^_]*/)
+      instance_variable_set(attr_name, default_value)
+    end
+  end
+
+  # SimpleModelがincludeされたとき、ClassMethodsを特異メソッドとして追加
+  def self.included(klass)
+    klass.extend ClassMethods
+  end
+
+  module ClassMethods
+    def attr_accessor(*attrs)
+      attrs.each do |attr|
+        # setter
+        define_method "#{attr}=" do |value|
+          # @{attr} に値をセット
+          instance_variable_set("@#{attr}", value)
+          # @{attr}_changed_history の操作履歴の最後に値を追加
+          attr_changed_history = instance_variable_get("@#{attr}_changed_history")
+          instance_variable_set("@#{attr}_changed_history", attr_changed_history.push(value))
+        end
+
+        # getter
+        define_method "#{attr}" do
+          instance_variable_get("@#{attr}")
+        end
+
+        # #{attr}_changed?
+        define_method "#{attr}_changed?" do
+          # @{attr}_changed_historyの要素が2以上あれば変更されているとみなす
+          instance_variable_get("@#{attr}_changed_history").count >= 2
+        end
+      end
+    end
+  end
 end
